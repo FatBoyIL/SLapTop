09495892621f8cdf541378745fa379f1b4da48eb
Jack Nguyen <huynguyenhonggia2703@gmail.com>
2024-12-15 00:12:23 +0700

    p7 1

--- src/main/java/com/example/laptopgiahuy2/controller/UserController.java
+++ src/main/java/com/example/laptopgiahuy2/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.example.laptopgiahuy2.controller;
 
+import com.example.laptopgiahuy2.model.Cart;
 import com.example.laptopgiahuy2.model.Category;
 import com.example.laptopgiahuy2.model.UserDtls;
+import com.example.laptopgiahuy2.repository.CartRepository;
+import com.example.laptopgiahuy2.service.CartService;
 import com.example.laptopgiahuy2.service.CategoryService;
 import com.example.laptopgiahuy2.service.UserDtlsService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +24,11 @@ import java.util.List;
 public class UserController {
     private UserDtlsService userDtlsService;
     private CategoryService categoryService;
-    public UserController(UserDtlsService userDtlsService, CategoryService categoryService) {
+    private CartService cartService;
+    public UserController(UserDtlsService userDtlsService, CategoryService categoryService, CartService cartService) {
         this.userDtlsService = userDtlsService;
         this.categoryService = categoryService;
+        this.cartService = cartService;
     }
     @ModelAttribute
     public void getUserDetails(Principal principal, Model model) {
@@ -36,5 +43,17 @@ public class UserController {
     @GetMapping("/")
     public String home() {
         return "user/home";
+    }
+
+    @GetMapping("/addCart")
+    public String addCart(@RequestParam Integer pid, @RequestParam Integer uid, HttpSession session) {
+        Cart cart =cartService.saveCart(pid,uid);
+
+        if (ObjectUtils.isEmpty(cart)) {
+            session.setAttribute("errorMsg", "Product add to cart failed");
+        } else {
+            session.setAttribute("succMsg", "Product added to cart");
+        }
+        return "redirect:/product/"+pid;
     }
 }
